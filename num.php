ae976457d3655016944f646c1d7283668a0f8d67
Alex Ivas <goodguyfc@protonmail.com>
2019-01-29 09:03:27 +0000

    some of the basic php methods

---
+++ num.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <?php
+        $num = 10;
+        $num++;
+        echo $num;
+        ?>
+        <?php
+        $num += 25;
+        echo $num;
+        ?>
+        <?php
+        echo abs(-100);
+        ?>
+        <!--na kvadrat-->
+        <?php
+        echo pow(2, 4);
+        ?>
+        <!--root-->
+        <?php
+        echo sqrt(16);
+        ?>
+        <!--number rounding-->
+        <?php
+        echo ceil(3.4);
+        ?>
+        <?php
+        echo floor(3.4);
+        ?>
+    
+</body>
+</html>
